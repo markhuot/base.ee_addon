ce1b27a27fb8a30f61b5f481a298d6d0d90a7ba5
Mark Huot <markhuot@MacPro250GB.local>
2011-03-01 22:39:04 -0500

    adding helper functions

--- mcp.base.php
+++ mcp.base.php
@@ -9,6 +9,21 @@ class Base_mcp {
 	
 	public function index()
 	{
+		$this->EE->cp->set_variable('cp_page_title', $this->EE->lang->line('base'));
+		
+		$this->EE->cp->set_breadcrumb(
+			BASE.AMP.'C=addons_modules'.AMP.'M=show_module_cp'.AMP.'module=base',
+			$this->EE->lang->line('base_module_name')
+		);
+		
+		$this->EE->cp->set_right_nav(array(
+			'add_record' => BASE.AMP.
+			                'C=addons_modules'.AMP.
+			                'M=show_module_cp'.AMP.
+			                'module=base'.AMP.
+			                'method=add_record'
+		));
+		
 		return 'Hello World!';
 	}
 }
