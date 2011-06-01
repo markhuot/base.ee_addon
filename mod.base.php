e1f93509b643ef68993256892b4411b087278e29
Mark Huot <mark@markhuot.com>
2011-06-01 16:56:35 -0400

    adding view loading

--- mod.base.php
+++ mod.base.php
@@ -8,4 +8,14 @@ class Base
 		$this->EE =& get_instance();
 	}
 	
+	public function entries()
+	{
+		$original_view_path = $this->EE->load->_ci_view_path;
+		$this->EE->load->_ci_view_path = PATH_THIRD.'base/views/';
+		
+		// $this->EE->load->view();
+		
+		$this->EE->load->_ci_view_path = $original_view_path;
+	}
+	
 }
